0bce3f208b0c08549095e4c031167301d8e476ba
saheer456 <khansaheer424@gmail.com>
2026-01-10 23:44:46 +0530

    Apply nuclear scroll fix to force native scrolling behavior

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import './scroll-fix.css'
 
 import ErrorBoundary from './components/ErrorBoundary'
 
